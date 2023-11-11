209deacb37deb7897a4c275f5e38918809ca60cb
robertbodrug <robertobodrug@gmail.com>
2023-11-11 13:43:01 +0100

    final

---
+++ src/main/java/com/example/Human.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Human {
+    private String name;
+    private String lastName;
+
+    public Human(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        System.out.println(        new Gson().toJson(new Human("Roberto","Bodrug")));
+    }
+}
